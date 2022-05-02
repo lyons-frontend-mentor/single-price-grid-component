198df0bb94d991d2d49d65ba0ee0652d91e001a2
Greg Lyons <gregory.lyons512@gmail.com>
2022-05-02 15:52:30 -0500

    Finished styling.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: "#2ab3b1",
+        yellow: "#c0df33",
+        lightgray: "#e6eff5",
+        bluegray: "#9aa7be"
+      }
+    },
   },
   plugins: [],
 }
